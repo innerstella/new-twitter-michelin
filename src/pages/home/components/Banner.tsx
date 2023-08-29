98fb6a6beb31342a4ff850211874f2b3531388e2
innerstella <suji0509@naver.com>
2023-08-29 15:18:52 +0900

    ✨ 배너 추가

--- src/pages/home/components/Banner.tsx
+++ src/pages/home/components/Banner.tsx
@@ -20,13 +20,16 @@ const Banner = () => {
     <BannerContainer>
       <Swiper {...bannerSettings}>
         <SwiperSlide>
+          <img className="banner" src="/assets/banner-mino.jpeg" alt="배너" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="banner" src="/assets/banner-ksy.png" alt="배너" />
+        </SwiperSlide>
+        <SwiperSlide>
           <img className="banner" src="/assets/banner-163wins.png" alt="배너" />
         </SwiperSlide>
         <SwiperSlide>
           <img className="banner" src="/assets/banner-jang-1.jpeg" alt="배너" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className="banner" src="/assets/banner-jang-2.jpeg" alt="배너" />
         </SwiperSlide>
         {/* <SwiperSlide>
           <img className="banner" src="/assets/banner-euilee.png" alt="배너" />
