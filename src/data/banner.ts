47242d2110b2194aae282fa51ceda771b90cb19c
innerstella <suji0509@naver.com>
2026-03-11 21:56:29 +0900

    add: cafe banner

--- src/data/banner.ts
+++ src/data/banner.ts
@@ -11,4 +11,10 @@ export const BANNER_LIST = [
     link: 'https://naver.me/xP8m35A2',
     name: '토마루해물칼국수',
   },
+  {
+    id: 3,
+    image: '/assets/banner/260311.webp',
+    link: 'https://naver.me/FaOZVLXm',
+    name: '세리오',
+  },
 ];
